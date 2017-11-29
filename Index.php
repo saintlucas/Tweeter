c108288d43f95076292f67eee8f86bedf685d21d
Łukasz <Lukasz@MacBook-Air-Lukas.local>
2017-11-29 11:10:20 +0100

    next steps

--- Index.php
+++ Index.php
@@ -1,13 +1,33 @@
 <?php
 
-require_once 'src/User1.php';
-require_once 'src/Tweet.php';
-require_once 'DBConnection.php';
-
 session_start();
 
 if (!isset($_SESSION['user_id'])) {
     header('Location: LoginSite.php'); //system logowania - Przekierowanie użytkownika pod dowolny adres
+}
+
+require_once 'src/User1.php';
+require_once 'src/Tweet.php';
+require_once 'DBConnection.php';
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['tweetToAdd']) && isset($_SESSION['userId'])) {
+        
+        require_once 'src/Tweet.php';
+        require_once 'DBconnection.php';
+        
+        $tweetToAdd = $_POST['tweetToAdd'];
+        $currentUserId = $_SESSION['userId'];
+        $tweet = new Tweet();
+        $tweet->setUserId($currentUserId);
+        $tweet->setTweet($tweetToAdd);
+        if ($tweet->saveTheTweetToTheDB($conn)) {
+            
+        } else {
+            echo 'blad';
+        }
+    }
 }
 
 
--- UserWebSite.php
+++ UserWebSite.php
@@ -1,12 +1,61 @@
 <?php
+session_start();
 
 require_once 'src/User1.php';
 require_once 'DBConnection.php';
 require_once 'Tweet.php';
 
-session_start();
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (isset($_GET['userId'])) {
+        $userId = $_GET['userId'];
 
-if (!isset($_SESSION['user_id'])){
-    header('Location: loginSite.pžhp');
+
+        $allTweets = Tweet::showAllTweets($conn, $userId);
+        $userNameToGet = User1::loadUserById($conn, $userId);
+        $username = $usernameToGet->getUsername();
+        $receiverId = $usernameToGet->getId();
+    }
 }
+?>
 
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+
+
+
+        <?php
+        echo 'Tweety użytkownika ' . $username . ' :' . ('<br>');
+
+        $allTweets = Tweet::loadAllTweetByUserId($conn, $_SESSION['userId']);
+        ?>
+
+
+
+        <ul>
+            <li><a href="Index.php">Strona główna</a></li>
+            <li><a href="UserWebSite.php">Strona użytkownika</a></li>
+            <li><a href="ShowPost.php">Wiadomości</a></li>
+            <li><a href="LogoutSite.php">Wyloguj się</a></li>
+        </ul>
+
+        <h1> Witaj <?php echo User1::loadUserById($conn, $_SESSION['userId'])->getName(); ?></h1>
+
+        <?php
+        foreach ($allTweets as $value) {
+            echo '<tr>';
+            echo "<td>" . $value->getId() . "</td>";
+            echo "<td>" . $value->getText() . "</td>";
+            echo "<td>" . User::returnUserNameById($conn, $value->getUserId());
+            echo '<br><a href="post.php?id=' . $value->getId() . '">Wyświetl post</a>';
+            echo '</td>';
+            echo "<td>" . $value->getCreationDate() . "</td>";
+            echo '</tr>';
+        }
+        ?>
+
+    </body>
+</html>
